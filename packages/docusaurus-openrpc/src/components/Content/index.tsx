c1c56f021f384906055d4a5008d953c2638a32ac
iosh <ioshx@foxmail.com>
2024-12-28 18:40:25 +0800

    chore: update

--- packages/docusaurus-openrpc/src/components/Content/index.tsx
+++ packages/docusaurus-openrpc/src/components/Content/index.tsx
@@ -11,95 +11,17 @@ import Examples from "../Examples";
 import "./index.css";
 import { Result } from "../Result";
 import Playground from "../Playground";
-import Form from "@rjsf/bootstrap-4";
-import validator from "@rjsf/validator-ajv8";
-import { RJSFSchema, UiSchema } from "@rjsf/utils";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 interface Props {
   method: MethodObject;
   servers: { name: string; url: string }[];
 }
 
-const convertOpenApiSchemaToJsonSchema = (schema: any): any => {
-  if (!schema) return schema;
-
-  let convertedSchema = { ...schema };
-
-  if (convertedSchema.nullable === true) {
-    if (convertedSchema.type) {
-      convertedSchema.type = Array.isArray(convertedSchema.type)
-        ? [...convertedSchema.type, "null"]
-        : [convertedSchema.type, "null"];
-    }
-
-    delete convertedSchema.nullable;
-  }
-
-  if (convertedSchema.properties) {
-    const newProperties: Record<string, any> = {};
-    Object.keys(convertedSchema.properties).forEach((key) => {
-      newProperties[key] = convertOpenApiSchemaToJsonSchema(
-        convertedSchema.properties[key]
-      );
-    });
-    convertedSchema.properties = newProperties;
-  }
-
-  if (convertedSchema.items) {
-    convertedSchema.items = convertOpenApiSchemaToJsonSchema(
-      convertedSchema.items
-    );
-  }
-
-  return convertedSchema;
-};
-
-const createJsonSchema = (params: MethodObjectParams): RJSFSchema => {
-  const properties: Record<string, JSONSchema> = {};
-  const required: string[] = [];
-
-  params.forEach((element) => {
-    if ("$ref" in element) {
-      return;
-    }
-
-    // 转换 schema
-    properties[element.name] = convertOpenApiSchemaToJsonSchema(element.schema);
-    if (element.required) {
-      required.push(element.name);
-    }
-  });
-
-  return {
-    type: "object" as const,
-    properties: properties,
-    required: required,
-  };
-};
-
-const uiSchema: UiSchema = {
-  "ui:description": "",
-  "ui:submitButtonOptions": {
-    norender: true,
-  },
-  
-
-  
-};
-
 const Content = ({ method, servers }: Props) => {
+  console.log(method);
   return (
     <div>
-      {method.params && (
-        <Form
-          liveValidate
-          uiSchema={uiSchema}
-          schema={createJsonSchema(method.params)}
-          validator={validator}
-          onChange={console.log}
-          showErrorList={false}
-        />
-      )}
       <p>{method.summary}</p>
       <Playground method={method} servers={servers} />
       <Params params={method.params} />
--- packages/docusaurus-openrpc/src/components/Playground/codes.ts
+++ packages/docusaurus-openrpc/src/components/Playground/codes.ts
@@ -1,12 +1,12 @@
 export interface CodeExample {
   name: string;
-  getCode: (url: string, method: string) => string;
+  getCode: (url: string, method: string, params: string[]) => string;
   language: string;
 }
 
 export const Curl: CodeExample = {
   name: "curl",
-  getCode: (url: string, method: string) => `curl --request POST \\
+  getCode: (url, method, params = []) => `curl --request POST \\
      --url ${url} \\
      --header 'accept: application/json' \\
      --header 'content-type: application/json' \\
@@ -14,11 +14,9 @@ export const Curl: CodeExample = {
 {
   "id": 1,
   "jsonrpc": "2.0",
-  "params": [
-    "string"
-  ],
+  "params": ${JSON.stringify(params)},
   "method": "${method}"
-}`,
+}'`,
   language: "javascript",
 };
 
