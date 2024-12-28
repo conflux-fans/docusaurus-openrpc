30839c478aadbfe9967898114dd152d3b54a7813
iosh <ioshx@foxmail.com>
2024-12-28 17:46:40 +0800

    chore: update

--- packages/docusaurus-openrpc/src/components/Playground/index.tsx
+++ packages/docusaurus-openrpc/src/components/Playground/index.tsx
@@ -3,10 +3,11 @@ import Codes from "./codes";
 import Tabs from "@theme-original/Tabs";
 import TabItem from "@theme-original/TabItem";
 import CodeBlock from "@theme-original/CodeBlock";
+import { MethodObject } from "@open-rpc/meta-schema";
 
 interface Props {
   servers: { name: string; url: string }[];
-  method: string;
+  method: MethodObject;
 }
 
 const Playground = ({ servers, method }: Props) => {
@@ -16,13 +17,16 @@ const Playground = ({ servers, method }: Props) => {
 
   return (
     <div>
+     
+
+
       <Tabs>
         {Codes.map((code, index) => {
           return (
             <TabItem key={index} value={code.name} label={code.name}>
               <div>
                 <CodeBlock language={code.language} showLineNumbers>
-                  {code.getCode(url, method)}{" "}
+                  {code.getCode(url, method.name)}{" "}
                 </CodeBlock>
               </div>
             </TabItem>
